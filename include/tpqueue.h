2497074902dab006808f4c2b9058cf188f988566
Rtiemm <99262095+Rtiemm@users.noreply.github.com>
2022-05-22 16:15:59 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,15 +1,56 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
-
 template<typename T>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+class TPQueue{
+ private:
+struct ITEM {
+    T vv;
+    ITEM* next;
 };
+ITEM* hh;
+ITEM* tl;
 
+ public:
+    TPQueue() :hh(nullptr), tl(nullptr) {}
+T pop() {
+    ITEM* tt = hh;
+    hh = hh->next;
+    return (tt->vv);
+}
+void push(T symbol) {
+    ITEM* tl = hh;
+    if (tl == nullptr) {
+        tl = new ITEM;
+        hh = tl;
+        tl->vv = symbol;
+        tl->next = nullptr;
+    } else {
+        ITEM* elem = new ITEM;
+        elem->vv = symbol;
+        if (hh->vv.prior < elem->vv.prior) {
+            eelm->next = tl;
+            hh = elem;
+        }
+        while (tl != nullptr) {
+        if ((tl->vv.prior == elem->vv.prior &&
+           (tl->next == nullptr ||
+            tl->next->vv.prior < elem->vv.prior)) ||
+           (tl->vv.prior > elem->vv.prior &&
+           ((tl->next != nullptr &&
+           tl->next->vv.prior < elem->vv.prior)||
+           (tl->next == nullptr)))) {
+          elem->next = tl->next;
+          tl->next = elem;
+          return;
+        }
+        tl = tl->next;
+      }
+    }
+}
+};
 struct SYM {
-  char ch;
-  int prior;
+    char ch;
+    int prior;
 };
-
 #endif  // INCLUDE_TPQUEUE_H_
